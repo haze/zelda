6ca0a2e2f6fe766b36b3ee12d251664edbdd9118
Haze Booth <isnt@haze.cool>
2021-09-16 17:58:13 -0400

    uncomment out tests

--- src/tests.zig
+++ src/tests.zig
@@ -37,33 +37,33 @@ const TestDataStruct = struct {
     maximum_bruh_enabled: bool,
 };
 
-// test "post some json data and get it back" {
-//     defer zelda.cleanup();
-//
-//     var source = TestDataStruct{
-//         .number_of_bruhs = 69,
-//         .bruh_status = "engaged",
-//         .maximum_bruh_enabled = true,
-//     };
-//     var httpBinResponse = try zelda.postJsonAndParseResponse(HTTPBinResponse, "https://httpbin.org/post", source, .{
-//         .allocator = std.testing.allocator,
-//         .parseOptions = .{ .ignore_unknown_fields = true },
-//     });
-//     defer std.json.parseFree(HTTPBinResponse, httpBinResponse, .{
-//         .allocator = std.testing.allocator,
-//         .ignore_unknown_fields = true,
-//     });
-//
-//     var obj = try std.json.parse(TestDataStruct, &std.json.TokenStream.init(httpBinResponse.data), .{
-//         .allocator = std.testing.allocator,
-//         .ignore_unknown_fields = true,
-//     });
-//     defer std.json.parseFree(TestDataStruct, obj, .{
-//         .allocator = std.testing.allocator,
-//         .ignore_unknown_fields = true,
-//     });
-//
-//     try std.testing.expectEqual(source.number_of_bruhs, obj.number_of_bruhs);
-//     try std.testing.expectEqual(source.maximum_bruh_enabled, obj.maximum_bruh_enabled);
-//     try std.testing.expectEqualStrings(source.bruh_status, obj.bruh_status);
-// }
+test "post some json data and get it back" {
+    defer zelda.cleanup();
+
+    var source = TestDataStruct{
+        .number_of_bruhs = 69,
+        .bruh_status = "engaged",
+        .maximum_bruh_enabled = true,
+    };
+    var httpBinResponse = try zelda.postJsonAndParseResponse(HTTPBinResponse, "https://httpbin.org/post", source, .{
+        .allocator = std.testing.allocator,
+        .parseOptions = .{ .ignore_unknown_fields = true },
+    });
+    defer std.json.parseFree(HTTPBinResponse, httpBinResponse, .{
+        .allocator = std.testing.allocator,
+        .ignore_unknown_fields = true,
+    });
+
+    var obj = try std.json.parse(TestDataStruct, &std.json.TokenStream.init(httpBinResponse.data), .{
+        .allocator = std.testing.allocator,
+        .ignore_unknown_fields = true,
+    });
+    defer std.json.parseFree(TestDataStruct, obj, .{
+        .allocator = std.testing.allocator,
+        .ignore_unknown_fields = true,
+    });
+
+    try std.testing.expectEqual(source.number_of_bruhs, obj.number_of_bruhs);
+    try std.testing.expectEqual(source.maximum_bruh_enabled, obj.maximum_bruh_enabled);
+    try std.testing.expectEqualStrings(source.bruh_status, obj.bruh_status);
+}
